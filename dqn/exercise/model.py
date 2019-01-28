803acaae4e3eddcbecff84c92a683e808c1aa67d
Bo QU <quboanthony@gmail.com>
2019-01-28 23:57:00 +0800

    first implements

--- dqn/exercise/model.py
+++ dqn/exercise/model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, state_size, action_size, seed,fc1_size=64,fc2_size=64):
         """Initialize parameters and build model.
         Params
         ======
@@ -16,7 +16,11 @@ class QNetwork(nn.Module):
         super(QNetwork, self).__init__()
         self.seed = torch.manual_seed(seed)
         "*** YOUR CODE HERE ***"
-
+        self.fc1=nn.Linear(state_size,fc1_size)
+        self.fc2=nn.Linear(fc1_size,fc2_size)
+        self.fc3=nn.Linear(fc2_size,action_size)
     def forward(self, state):
         """Build a network that maps state -> action values."""
-        pass
+        x=F.relu(self.fc1(state))
+        x=F.relu(self.fc2(x))
+        return self.fc3(x)
